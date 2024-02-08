587f8dfa93d62cb1de6cc5f2d424537212adb910
Morten Bendeke <mbendeke@hotmail.com>
2024-02-08 13:30:57 +0100

    initial main

---
+++ src/components/frontpage.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Frontpage extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hi</h1>
+            </div>
+        );
+    }
+}
+
+Frontpage.propTypes = {};
+
+export default Frontpage;
