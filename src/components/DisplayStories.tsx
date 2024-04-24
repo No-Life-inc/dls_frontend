52dd08e470252d1785bb62b2233dae9ba93124e2
Morten Bendeke <mbendeke@hotmail.com>
2024-04-24 11:50:55 +0200

    edit story component

--- src/components/DisplayStories.tsx
+++ src/components/DisplayStories.tsx
@@ -2,22 +2,31 @@ import React, { useState, useEffect } from 'react'
 import { Story } from '../types/types'
 import { GETALLSTORIES } from "../graphql/queries";
 import {useQuery} from '@apollo/client';
+import EditStory from './EditStory';
+
 
 /***
  * This component displays a list of stories.
  */
+
+
 const DisplayStories = () =>{
     const { loading, error, data } = useQuery(GETALLSTORIES);
     const [stories, setStories] = useState<Story[]>([]);
-
+    const [editingStory, setEditingStory] = useState<Story | null>(null);
+    
     useEffect(() => {
       if (data) {
         setStories(data.getAllStories);
       }
     }, [data]);
-
+    
     if (loading) return <p>"Loading..."</p>;
     if (error) return <p>Error : {error.message}</p>; 
+
+    const handleEdit = (story: Story) => {
+      setEditingStory(story);
+    };
 
   return (
     <div>
@@ -26,10 +35,12 @@ const DisplayStories = () =>{
                 {stories.map((story: Story) => (
     <li key={story._id}>
         {story.storyInfo?.bodyText} - {story.storyInfo?.imgUrl}
+        <button onClick={() => handleEdit(story)}>Edit</button>
     </li>
-))}
-                </ul>
-                </div>
+    ))}
+    </ul>
+    {editingStory && <EditStory story={editingStory}/>}
+    </div>
   )
 }
 
